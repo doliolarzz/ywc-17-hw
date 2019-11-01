afabbbe41d5656f54966d50d08e09b71687f53af
Warit Prasartritha <doliolarzz@hotmail.com>
2019-11-02 02:46:33 +0700

    Updates

---
+++ precache-manifest.2a29e8ef663fdd26d4a5f93bdf40fdaf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287af0a4d4f500d28edfba6287c679ad",
+    "url": "/ywc-17-hw/index.html"
+  },
+  {
+    "revision": "c3f2c2dc169615e34147",
+    "url": "/ywc-17-hw/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "11a0f45e16ba3355c4b3",
+    "url": "/ywc-17-hw/static/css/main.21e34cdb.chunk.css"
+  },
+  {
+    "revision": "c3f2c2dc169615e34147",
+    "url": "/ywc-17-hw/static/js/2.8d9640ad.chunk.js"
+  },
+  {
+    "revision": "11a0f45e16ba3355c4b3",
+    "url": "/ywc-17-hw/static/js/main.bc942b42.chunk.js"
+  },
+  {
+    "revision": "624f65172bb9e8346543",
+    "url": "/ywc-17-hw/static/js/runtime-main.a371f850.js"
+  },
+  {
+    "revision": "398d843b5815a120bc15d604003b7dde",
+    "url": "/ywc-17-hw/static/media/1.398d843b.png"
+  },
+  {
+    "revision": "34f69a5bf778b186c5820c69466ecf2d",
+    "url": "/ywc-17-hw/static/media/2.34f69a5b.png"
+  },
+  {
+    "revision": "093f4f579ff0882a2200c8860e411066",
+    "url": "/ywc-17-hw/static/media/3.093f4f57.png"
+  },
+  {
+    "revision": "8b71f26b142425c9db5075c674c7fe5e",
+    "url": "/ywc-17-hw/static/media/4.8b71f26b.png"
+  },
+  {
+    "revision": "094779696e270e825510a8e0e5de0cd9",
+    "url": "/ywc-17-hw/static/media/5.09477969.png"
+  },
+  {
+    "revision": "2529bd05f4a98e5215edc2d91f0597bb",
+    "url": "/ywc-17-hw/static/media/6.2529bd05.png"
+  },
+  {
+    "revision": "9e7d7f4a11dd5276a2b04c6c17ac89c2",
+    "url": "/ywc-17-hw/static/media/banner-center.9e7d7f4a.png"
+  },
+  {
+    "revision": "1b48104080cd8f7e3dd700dc98c70745",
+    "url": "/ywc-17-hw/static/media/banner-left.1b481040.png"
+  },
+  {
+    "revision": "b28170f021a607a59c649ae7b8ff8cf5",
+    "url": "/ywc-17-hw/static/media/banner-right.b28170f0.png"
+  },
+  {
+    "revision": "0b9c7c9cd693cf575501518e1a493198",
+    "url": "/ywc-17-hw/static/media/banner.0b9c7c9c.png"
+  },
+  {
+    "revision": "ed683f12a7722cf108f1f5e78c8e38bf",
+    "url": "/ywc-17-hw/static/media/c1.ed683f12.png"
+  },
+  {
+    "revision": "6c42699db83442be94bd7a7340c205a6",
+    "url": "/ywc-17-hw/static/media/c2.6c42699d.png"
+  },
+  {
+    "revision": "1b47ec360e44880a573806ea4c899619",
+    "url": "/ywc-17-hw/static/media/c3.1b47ec36.png"
+  }
+]);
