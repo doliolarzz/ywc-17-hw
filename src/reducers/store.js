b1bcce18963d0225a307095cbeac80ebd5307c5b
Warit Prasartritha <doliolarzz@hotmail.com>
2019-11-02 12:55:12 +0700

    disable logger

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -3,7 +3,7 @@ import thunkMiddleware from 'redux-thunk';
 // import { createLogger } from 'redux-logger';
 import rootReducer from './index';
 
-const loggerMiddleware = createLogger();
+// const loggerMiddleware = createLogger();
 
 export default createStore(
     rootReducer,
